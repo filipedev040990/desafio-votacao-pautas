361143b8a5184dc59a29c998a904874232ce5a35
Filipe Siqueira <filipedev040990@gmail.com>
2024-09-18 11:37:26 -0300

    feat: add voting session gateway getbyid method

--- src/adapters/gateways/voting-session.gateway.ts
+++ src/adapters/gateways/voting-session.gateway.ts
@@ -1,8 +1,17 @@
-import { CreateVotingSessionInputDTO, VotingSessionGatewayInterface } from '@/domain/gateways/voting-session-gateway.interface'
+import {
+  CreateVotingSessionInputDTO,
+  CreateVotingSessionOutputDTO,
+  VotingSessionGatewayInterface
+} from '@/domain/gateways/voting-session-gateway.interface'
 import { prismaClient } from './prisma-client'
 
 export class VotingSessionGateway implements VotingSessionGatewayInterface {
   async save(data: CreateVotingSessionInputDTO): Promise<void> {
     await prismaClient.votingSession.create({ data })
+  }
+
+  async getById(id: string): Promise<CreateVotingSessionOutputDTO | null> {
+    const votingSession = await prismaClient.votingSession.findFirst({ where: { id } })
+    return votingSession ?? null
   }
 }
